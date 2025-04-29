8a19a4edfc6f85cee37bd3545c15e424276c37eb
harshitit112 <hv702383@gmail.com>
2025-04-29 16:22:47 +0530

    feat: implement HomePage and AuthScreen components, add Footer, and update routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import LoginPage from "./pages/LoginPage.jsx"
-import SignUpPage from "./pages/SignUpPage.jsx"
-import HomePage from "./pages/HomePage.jsx"
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import HomePage from "./pages/home/HomePage";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/signup" element={<SignUpPage/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
