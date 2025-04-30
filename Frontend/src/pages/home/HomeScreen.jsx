e70dcc1b85e38da5234da542423feae27470cdc3
harshitit112 <hv702383@gmail.com>
2025-04-30 19:55:34 +0530

    feat: add MovieSlider component and integrate it into HomeScreen; update constants and package configurations

--- Frontend/src/pages/home/HomeScreen.jsx
+++ Frontend/src/pages/home/HomeScreen.jsx
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import { Link } from "react-router-dom";
 import { Info, Play } from "lucide-react";
 import useGetTrendingContent from "../../hooks/useGetTrendingContent";
-import { ORIGINAL_IMG_BASE_URL } from "../../utils/constants";
+import {
+  MOVIE_CATEGORIES,
+  ORIGINAL_IMG_BASE_URL,
+  TV_CATEGORIES,
+} from "../../utils/constants";
+import { useContentStore } from "../../store/content";
+import MovieSlider from "../../components/movieSlider";
 
 const HomeScreen = () => {
   const { trendingContent } = useGetTrendingContent();
 
-  console.log("trendingContent", trendingContent);
+  const { contentType } = useContentStore();
+
+  const [imgLoading, setImgLoading] = useState(true);
 
   if (!trendingContent)
-		return (
-			<div className='h-screen text-white relative'>
-				<Navbar />
-				<div className='absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer' />
-			</div>
-		);
+    return (
+      <div className="h-screen text-white relative">
+        <Navbar />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer" />
+      </div>
+    );
 
   const title = trendingContent.title || trendingContent.name || "Untitled";
   const year =
@@ -30,54 +38,73 @@ const HomeScreen = () => {
       : trendingContent?.overview ?? "";
 
   return (
-    <div className="relative h-screen text-white">
-      <Navbar />
+    <>
+      <div className="relative h-screen text-white">
+        <Navbar />
 
-      <img
-        src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
-        alt="hero"
-        className="absolute top-0 left-0 w-full h-full object-cover -z-50"
-      />
+        {/* { COOL OPTIMIZATION HACK FOR IMAGE } */}
+        {imgLoading && (
+          <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center shimmer -z-10" />
+        )}
 
-      <div
-        className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
-        aria-hidden="true"
-      />
+        <img
+          src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
+          alt="hero"
+          className="absolute top-0 left-0 w-full h-full object-cover -z-50"
+          onLoad={() => {
+            setImgLoading(false);
+          }}
+        />
 
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
-        <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
+          aria-hidden="true"
+        />
 
-        <div className="max-w-2xl">
-          <h1 className="mt-4 text-6xl font-extrabold">{title}</h1>
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
+          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10" />
 
-          <p className="mt-2 text-lg">
-            {year} | {rating}
-          </p>
+          <div className="max-w-2xl">
+            <h1 className="mt-4 text-6xl font-extrabold">{title}</h1>
 
-          <p className="mt-4 text-lg">{overview}</p>
-        </div>
+            <p className="mt-2 text-lg">
+              {year} | {rating}
+            </p>
 
-        <div className="flex mt-8">
-          <Link
-            to={`/watch/${trendingContent?.id}`}
-            className="bg-white hover:bg-white/50 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
-          >
-            <Play className="size-6 mr-2 fill-black" />
-            Play
-          </Link>
+            <p className="mt-4 text-lg">{overview}</p>
+          </div>
 
-          <Link
-            to={`/watch/${trendingContent?.id}`}
-            className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center"
-          >
-            <Info className="size-6 mr-2" />
-            More Info
-          </Link>
+          <div className="flex mt-8">
+            <Link
+              to={`/watch/${trendingContent?.id}`}
+              className="bg-white hover:bg-white/50 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
+            >
+              <Play className="size-6 mr-2 fill-black" />
+              Play
+            </Link>
+
+            <Link
+              to={`/watch/${trendingContent?.id}`}
+              className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center"
+            >
+              <Info className="size-6 mr-2" />
+              More Info
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="flex flex-col gap-10 bg-black py-10">
+        {contentType === "movie"
+          ? MOVIE_CATEGORIES.map((category) => (
+              <MovieSlider key={category} category={category} />
+            ))
+          : TV_CATEGORIES.map((category) => (
+              <MovieSlider key={category} category={category} />
+            ))}
+      </div>
+    </>
   );
 };
 
 export default HomeScreen;
-
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,12 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { plugin } from "mongoose";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), plugin[tailwindScrollbarHide]],
   server: {
     proxy: {
       "/api": {
