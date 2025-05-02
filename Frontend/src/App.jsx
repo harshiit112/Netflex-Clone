d450cffa40f36f254bcb4df503014f8d958a835b
harshitit112 <hv702383@gmail.com>
2025-05-02 15:12:49 +0530

    feat: add SearchPage and SearchHistoryPage components; implement routing in App component; create NotFoundPage; enhance error handling and styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,9 @@ import { useAuthStore } from "./store/authUser.js";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import WatchPage from "./pages/WatchPage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
+import SearchHistoryPage from "./pages/SearchHistoryPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 function App() {
 
@@ -36,6 +39,9 @@ function App() {
         <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />}/>
         <Route path="/signup" element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+        <Route path="/search" element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path="/history" element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+        <Route path="/*" element={<NotFoundPage />}/>
       </Routes>
       <Footer />
 
@@ -45,3 +51,18 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
